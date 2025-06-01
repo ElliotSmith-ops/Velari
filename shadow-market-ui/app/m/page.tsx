0840d44f20a02017b7c126f98c40f7c02fd122a5
ElliotsmithUSC <elliotas@usc.edu>
2025-06-01 14:01:34 -0700

    mobile buy credit ui5

--- shadow-market-ui/app/m/page.tsx
+++ shadow-market-ui/app/m/page.tsx
@@ -53,9 +53,9 @@ export default function MobileHomePage() {
         <Image
           src="/surfrider-logo.png"
           alt="SurfRider Logo"
-          width={180}
-          height={60}
-          className="h-10 w-auto"
+          width={240}
+          height={80}
+          className="h-20 w-auto"
         />
       </div>
 
