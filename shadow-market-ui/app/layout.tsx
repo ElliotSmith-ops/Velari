6646b6399529208e8edafc688aa72471ba946645
ElliotsmithUSC <elliotas@usc.edu>
2025-06-03 15:03:43 -0700

    google tags 4

--- shadow-market-ui/app/layout.tsx
+++ shadow-market-ui/app/layout.tsx
@@ -36,7 +36,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="dark overflow-x-hidden">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+      <head>
+  <link rel="icon" href="/favicon.ico" sizes="any" />
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-MWV2EHMNBG"></script>
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-MWV2EHMNBG');
+      `,
+    }}
+  />
+</head>
+
         {/* Optional: Support for PNG or SVG */}
         {/* <link rel="icon" type="image/png" href="/favicon.png" /> */}
       </head>
